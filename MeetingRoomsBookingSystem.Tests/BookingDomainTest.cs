852b91d3bfa63e7cfb2ee2ee6f867038b8a8f2dd
Carmel Raj Melkior <WK29@eurofins.com>
2022-06-01 15:20:46 +0530

    Build error fix

--- MeetingRoomsBookingSystem.Tests/BookingDomainTest.cs
+++ MeetingRoomsBookingSystem.Tests/BookingDomainTest.cs
@@ -7,12 +7,9 @@ namespace MeetingRoomsBookingSystem.Tests
     public class BookingDomainTest
     {
 
-        private readonly Mock<IBookingDomainContract> _bookingModel;
-
         [SetUp]
         public void Setup()
         {
-            _bookingModel = new Mock<IBookingDomainContract>();
         }
 
         [Test]
